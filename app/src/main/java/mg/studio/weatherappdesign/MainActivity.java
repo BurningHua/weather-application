700e3cba6c228c5c4a460ecdb67d838ebdab6bf1
BurningHua <47785443+BurningHua@users.noreply.github.com>
2019-03-04 23:15:09 +0800

    Add files via upload

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -1,12 +1,18 @@
 package mg.studio.weatherappdesign;
 
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +22,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DownloadUpdate().execute();
     }
 
     public void btnClick(View view) {
         new DownloadUpdate().execute();
+
     }
 
 
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            String stringUrl = "https://mpianatra.com/Courses/info.txt";
+            String stringUrl = "https://cqu.andream.app/weather";
             HttpURLConnection urlConnection = null;
             BufferedReader reader;
 
@@ -72,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 //The temperature
                 return buffer.toString();
 
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ProtocolException e) {
@@ -84,9 +94,174 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String temperature) {
+        protected void onPostExecute(String jsoninfo) {
             //Update the temperature displayed
-            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temperature);
+            if(jsoninfo == null){
+                    Toast.makeText(getApplicationContext(), "No network",Toast.LENGTH_LONG).show();}
+            else {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsoninfo);
+                    Log.d("TAG", jsonObject.getString("location"));
+                    JSONArray daily = jsonObject.getJSONArray("daily");
+                    JSONObject forecast = daily.getJSONObject(0);
+                    ((TextView) findViewById(R.id.tv_date)).setText(forecast.getString("date"));
+                    ((TextView) findViewById(R.id.temperature_of_the_day)).setText(forecast.getString("high"));
+                    int code_day = forecast.getInt("code_day");
+                    ImageView imageView = (ImageView) findViewById(R.id.img_weather_condition);
+                    switch (code_day) {
+                        case 0:
+                            imageView.setImageResource(R.drawable.sunny_small);
+                            break;
+                        case 4:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 9:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 13:
+                            imageView.setImageResource(R.drawable.rainy_small);
+                            break;
+                        case 15:
+                            imageView.setImageResource(R.drawable.rainy_up);
+                            break;
+                        case 32:
+                            imageView.setImageResource(R.drawable.windy_small);
+                            break;
+                        default:
+                            break;
+                    }
+                    String week[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+                    Calendar cal = Calendar.getInstance();
+                    int i = cal.get(Calendar.DAY_OF_WEEK);
+                    ((TextView) findViewById(R.id.date_of_week)).setText(week[i - 1]);
+                    ((TextView) findViewById(R.id.next_day)).setText(week[i]);
+                    ((TextView) findViewById(R.id.next_twoday)).setText(week[i + 1]);
+                    ((TextView) findViewById(R.id.next_threeday)).setText(week[i + 2]);
+                    ((TextView) findViewById(R.id.next_fourday)).setText(week[i + 3]);
+
+                    forecast = daily.getJSONObject(1);
+                    code_day = forecast.getInt("code_day");
+                    imageView = (ImageView) findViewById(R.id.img_weather_condition1);
+                    switch (code_day) {
+                        case 0:
+                            imageView.setImageResource(R.drawable.sunny_small);
+                            break;
+                        case 4:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 9:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 13:
+                            imageView.setImageResource(R.drawable.rainy_small);
+                            break;
+                        case 15:
+                            imageView.setImageResource(R.drawable.rainy_up);
+                            break;
+                        case 32:
+                            imageView.setImageResource(R.drawable.windy_small);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    forecast = daily.getJSONObject(2);
+                    code_day = forecast.getInt("code_day");
+                    imageView = (ImageView) findViewById(R.id.img_weather_condition2);
+                    switch (code_day) {
+                        case 0:
+                            imageView.setImageResource(R.drawable.sunny_small);
+                            break;
+                        case 4:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 9:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 13:
+                            imageView.setImageResource(R.drawable.rainy_small);
+                            break;
+                        case 15:
+                            imageView.setImageResource(R.drawable.rainy_up);
+                            break;
+                        case 32:
+                            imageView.setImageResource(R.drawable.windy_small);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    forecast = daily.getJSONObject(3);
+                    code_day = forecast.getInt("code_day");
+                    imageView = (ImageView) findViewById(R.id.img_weather_condition3);
+                    switch (code_day) {
+                        case 0:
+                            imageView.setImageResource(R.drawable.sunny_small);
+                            break;
+                        case 4:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 9:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 13:
+                            imageView.setImageResource(R.drawable.rainy_small);
+                            break;
+                        case 15:
+                            imageView.setImageResource(R.drawable.rainy_up);
+                            break;
+                        case 32:
+                            imageView.setImageResource(R.drawable.windy_small);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    forecast = daily.getJSONObject(4);
+                    code_day = forecast.getInt("code_day");
+                    imageView = (ImageView) findViewById(R.id.img_weather_condition4);
+                    switch (code_day) {
+                        case 0:
+                            imageView.setImageResource(R.drawable.sunny_small);
+                            break;
+                        case 4:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 9:
+                            imageView.setImageResource(R.drawable.partly_sunny_small);
+                            break;
+                        case 13:
+                            imageView.setImageResource(R.drawable.rainy_small);
+                            break;
+                        case 15:
+                            imageView.setImageResource(R.drawable.rainy_up);
+                            break;
+                        case 32:
+                            imageView.setImageResource(R.drawable.windy_small);
+                            break;
+                        default:
+                            break;
+                    }
+                    Toast.makeText(getApplicationContext(), "Refresh successfully",Toast.LENGTH_LONG).show();
+
+
+                    //for (int i = 0; i < 5; i++) {
+                    //JSONObject forweather = forecast.getJSONObject(i);
+                    //Log.d("TAG", forweather.getString("high"));
+                    //System.out.println("姓名：" + forweather.getString("name"));
+                    //System.out.println("性别：" + forweather.getString("gender"));
+                    //System.out.println("年龄：" + forweather.getInt("age") + "\n");
+                    //}
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //((TextView) findViewById(R.id.temperature_of_the_day)).setText(jsoninfo);
+            //((ImageView)findViewById((R.id.img_weather_condition)).setBackground(sunny_small));_
         }
     }
+
+
 }
